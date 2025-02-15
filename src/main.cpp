0b146cba2682be59454179dc892695786608f25e
Kil3 <kriza9605@gmail.com>
2025-02-15 21:21:20 +0100

    Project setup with google tests and vcpkg package manager

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by kil3 on 2/15/25.
+//
+
+double sum(double a, double b) {
+  return a + b;
+}
+
+int main() {
+  return 0;
+}
---
+++ tests/test_setup.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kil3 on 2/15/25.
+//
+
+#include <gtest/gtest.h>
+#include "../src/main.cpp"  // Include the function we want to test
+
+TEST(SimpleTest, Test1) {
+    EXPECT_EQ(1, 1);  // This should always pass
+}
