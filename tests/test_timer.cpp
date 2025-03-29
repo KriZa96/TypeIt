dbfe9265cd5ad0e78f7601205cdc98aac3c41e89
Kil3 <kriza9605@gmail.com>
2025-03-29 12:23:43 +0100

    Test issue in pipeline, but no problem locally

--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -15,6 +15,7 @@ TEST(TimerTest, ElapsedTimeStartsAtZero) {
     Timer timer(10);
     GameState::game_session_in_progress = true;
     timer.started_timer_ = true;
+    GameState::game_finished = false;
     EXPECT_FLOAT_EQ(timer.get_elapsed_time(), 0);
 }
 
@@ -23,6 +24,7 @@ TEST(TimerTest, ElapsedTimeAfterDelay) {
     Timer timer(10);
     GameState::game_session_in_progress = true;
     timer.started_timer_ = true;
+    GameState::game_finished = false;
     std::this_thread::sleep_for(std::chrono::seconds(1));
     EXPECT_GE(timer.get_elapsed_time(), 1);
 }
@@ -32,6 +34,7 @@ TEST(TimerTest, ElapsedTimeAfterMaxTime) {
     Timer timer(1);
     GameState::game_session_in_progress = true;
     timer.started_timer_ = true;
+    GameState::game_finished = false;
     std::this_thread::sleep_for(std::chrono::seconds(2));
     EXPECT_GE(timer.get_elapsed_time(), 1);
 }
@@ -40,6 +43,7 @@ TEST(TimerTest, ElapsedTimeAfterMaxTime) {
 TEST(TimerTest, RemainingTimeImmediate) {
     Timer timer(10);
     GameState::game_session_in_progress = true;
+    GameState::game_finished = false;
     timer.started_timer_ = true;
     EXPECT_EQ(timer.get_time_left_str(), "10s");
 }
@@ -48,6 +52,7 @@ TEST(TimerTest, RemainingTimeImmediate) {
 TEST(TimerTest, RemainingTimeStringAfter1Sec) {
     Timer timer(10);
     GameState::game_session_in_progress = true;
+    GameState::game_finished = false;
     timer.started_timer_ = true;
     std::this_thread::sleep_for(std::chrono::seconds(1));
     EXPECT_EQ(timer.get_time_left_str(), "9s");
@@ -57,6 +62,7 @@ TEST(TimerTest, RemainingTimeStringAfter1Sec) {
 TEST(TimerTest, RemainingTimeStringAfterMaxTime) {
     Timer timer(1);
     GameState::game_session_in_progress = true;
+    GameState::game_finished = false;
     timer.started_timer_ = true;
     std::this_thread::sleep_for(std::chrono::seconds(2));
     EXPECT_EQ(timer.get_time_left_str(), "0s");
@@ -64,6 +70,7 @@ TEST(TimerTest, RemainingTimeStringAfterMaxTime) {
 
 TEST(TimerTest, DosentCalculateWhenStartGameFalse) {
     Timer timer(10);
+    GameState::game_finished = false;
     std::this_thread::sleep_for(std::chrono::seconds(2));
     EXPECT_EQ(timer.get_time_left_str(), "10s");
 }
