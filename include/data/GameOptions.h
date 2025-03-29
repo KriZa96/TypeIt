6ccf5e4fcf2443d01ff37a7bbba77fa26a2f0d90
KriZa96 <kriza9605@gmail.com>
2025-03-29 11:22:57 +0100

    Resolved default files paths

--- include/data/GameOptions.h
+++ include/data/GameOptions.h
@@ -5,6 +5,8 @@
 #ifndef GAMEOPTIONS_H
 #define GAMEOPTIONS_H
 
+#include <filesystem>
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -13,8 +15,14 @@ struct GameOptions {
     inline static int selected_radiobox_time_ = 0;
     inline static std::string custom_radiobox_input_;
 
+    inline static std::filesystem::path current_file_path = __FILE__;
+    inline static std::filesystem::path target_file_path =
+            current_file_path.parent_path().parent_path().parent_path() / "files";
     inline static std::vector<std::string> text_radiobox_values_{
-        "../../files/simple.txt", "../../files/medium.txt", "../../files/hard.txt", ""
+            (target_file_path / "simple.txt").string(),
+            (target_file_path / "medium.txt").string(),
+            (target_file_path / "hard.txt").string(),
+            ""
     };
     inline static int selected_radiobox_text_ = 0;
 };
