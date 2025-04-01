2c9e7b0745375c84c0f48e0b7b4b064c17745b1e
KriZa96 <kriza9605@gmail.com>
2025-04-01 16:25:34 +0200

    Encapsulated the get time element in Timer

--- include/core/Timer.h
+++ include/core/Timer.h
@@ -17,13 +17,14 @@ class Timer {
         int get_elapsed_time();
         std::string get_time_left_str();
         int& get_elapsed_time_reference();
-        ftxui::Element get_time_element();
         ftxui::Component get_time_component();
     private:
         int total_time_;
         int elapsed_time_;
         bool started_timer_;
         std::chrono::steady_clock::time_point start_time_;
+
+        ftxui::Element get_time_element();
 };
 
 
